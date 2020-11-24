fb6729538e567627ae0643d0d62bccfd51808d5e
Lucas Alves <lucasalvessm@outlook.com>
2020-11-24 20:36:41 -0300

    list app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView
+} from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState(new Array<string>());
+
+  const handleInputTextChanges = (text: string) => {
+    setEnteredGoal(text);
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals([...courseGoals, enteredGoal]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal" style={styles.input}
+          onChangeText={handleInputTextChanges}
+          value={enteredGoal}
+        />
+        <StatusBar style="auto" />
+        <Button title='Add' onPress={addGoalHandler}></Button>
+      </View>
+      <ScrollView>
+        {
+          courseGoals.map((goal, index) =>
+              <View style={styles.listItem} key={index}>
+                <Text>{goal}</Text>
+              </View>
+          )
+        }
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    paddingTop: 50
   },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    width: '80%',
+    padding: 5,
+    margin: 5
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  listItem: {
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    margin: 5
+  }
 });
